42e45946a54a4e2521d0e05166880f4f4954eb20
MichaelCummins <Michael.Cummins1@marist.edu>
2020-06-12 00:48:27 -0400

    Adding mana bar

--- UpdatedProject/Assets/EnterDoor.cs
+++ UpdatedProject/Assets/EnterDoor.cs
@@ -19,13 +19,21 @@ public class EnterDoor : MonoBehaviour
     private void OnTriggerEnter2D(Collider2D other) {
         if(other.gameObject.tag == "Player"){
             if(Direction == 1){
-                GameObject.Find("Main Camera").transform.position = new Vector3(currCameraPosition.x, currCameraPosition.y + 8, currCameraPosition.z);
+                if(Input.GetKeyDown("w")){
+                    GameObject.Find("Main Camera").transform.position = new Vector3(currCameraPosition.x, currCameraPosition.y + 8, currCameraPosition.z);
+                }
             } else if(Direction == 2){
-                GameObject.Find("Main Camera").transform.position = new Vector3(currCameraPosition.x, currCameraPosition.y - 8, currCameraPosition.z);
+                if(Input.GetKeyDown("s")){
+                    GameObject.Find("Main Camera").transform.position = new Vector3(currCameraPosition.x, currCameraPosition.y - 8, currCameraPosition.z);
+                }
             } else if(Direction == 3){
-                GameObject.Find("Main Camera").transform.position = new Vector3(currCameraPosition.x + 14, currCameraPosition.y, currCameraPosition.z);
+                if(Input.GetKeyDown("d")){
+                    GameObject.Find("Main Camera").transform.position = new Vector3(currCameraPosition.x + 14, currCameraPosition.y, currCameraPosition.z);
+                }
             } else if(Direction == 4){
-                GameObject.Find("Main Camera").transform.position = new Vector3(currCameraPosition.x - 14, currCameraPosition.y, currCameraPosition.z);
+                if(Input.GetKeyDown("a")){
+                    GameObject.Find("Main Camera").transform.position = new Vector3(currCameraPosition.x - 14, currCameraPosition.y, currCameraPosition.z);
+                }
             } else{
                 Debug.Log("This shouldnt be seen, a spawnpoint is missing a direction");
             }
