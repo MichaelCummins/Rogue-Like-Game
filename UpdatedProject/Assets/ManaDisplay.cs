d3b0ac0d4ffa0f4ca579433450f644faeaec73b1
MichaelCummins <Michael.Cummins1@marist.edu>
2020-06-16 16:59:43 -0400

    Mana bar connected to player now

---
+++ UpdatedProject/Assets/ManaDisplay.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ManaDisplay : MonoBehaviour
+{
+    public int currentMana;
+    public Image mana;
+    
+    void Start(){
+        mana.fillAmount = 0.5f;
+    }
+
+    void Update() {
+
+    }
+}
