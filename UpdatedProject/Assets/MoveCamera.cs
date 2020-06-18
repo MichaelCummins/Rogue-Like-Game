ee8a2f1625d9af169d2a5a1e3b99b2ab879631ea
MichaelCummins <Michael.Cummins1@marist.edu>
2020-06-18 14:36:57 -0400

    Camera will work today

--- UpdatedProject/Assets/MoveCamera.cs
+++ UpdatedProject/Assets/MoveCamera.cs
@@ -4,6 +4,21 @@ using UnityEngine;
 
 public class MoveCamera : MonoBehaviour
 {
+    public Collider2D northCollider;
+    public Collider2D southCollider;
+    public Collider2D westCollider;
+    public Collider2D eastCollider;
+
+    Vector3 currCameraPosition;
+
+    private void Start() {
+
+        currCameraPosition = GameObject.Find("Main Camera").transform.position;
+    }
+    
     private void OnTriggerEnter2D(Collider2D other) {
-    } 
+
+        //Debug.Log("Collider " + )
+        GameObject.Find("Main Camera").transform.position = new Vector3(currCameraPosition.x, currCameraPosition.y + 8, currCameraPosition.z);
+    }
 }
