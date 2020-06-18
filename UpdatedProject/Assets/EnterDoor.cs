d3ca900bc06fc034ea46b57ef1e9ecec9aee97fe
MichaelCummins <Michael.Cummins1@marist.edu>
2020-06-17 23:28:01 -0400

    I swear I will understand camera position at one point

--- UpdatedProject/Assets/EnterDoor.cs
+++ UpdatedProject/Assets/EnterDoor.cs
@@ -10,22 +10,30 @@ public class EnterDoor : MonoBehaviour
     private int rand;
     private RoomTemplates templates;
     Vector3 currCameraPosition;
+    Vector3 currPlayerPosition;
 
     private void Start() {
         templates = GameObject.FindGameObjectWithTag("Rooms").GetComponent<RoomTemplates>();
         currCameraPosition = GameObject.Find("Main Camera").transform.position;
     }
 
+    private void Update() {
+        currPlayerPosition = GameObject.Find("Player").transform.position;
+    }
     private void OnTriggerEnter2D(Collider2D other) {
         if(other.gameObject.tag == "Player"){
             if(Direction == 1){
                 GameObject.Find("Main Camera").transform.position = new Vector3(currCameraPosition.x, currCameraPosition.y + 8, currCameraPosition.z);
+                other.transform.position =  new Vector3(currPlayerPosition.x, currPlayerPosition.y + 3, currPlayerPosition.z);
             } else if(Direction == 2){
                 GameObject.Find("Main Camera").transform.position = new Vector3(currCameraPosition.x, currCameraPosition.y - 8, currCameraPosition.z);
+                other.transform.position =  new Vector3(currPlayerPosition.x, currPlayerPosition.y - 2, currPlayerPosition.z);
             } else if(Direction == 3){
                 GameObject.Find("Main Camera").transform.position = new Vector3(currCameraPosition.x + 14, currCameraPosition.y, currCameraPosition.z);
+                other.transform.position =  new Vector3(currPlayerPosition.x + 2, currPlayerPosition.y, currPlayerPosition.z);
             } else if(Direction == 4){
                 GameObject.Find("Main Camera").transform.position = new Vector3(currCameraPosition.x - 14, currCameraPosition.y, currCameraPosition.z);
+                other.transform.position =  new Vector3(currPlayerPosition.x - 2, currPlayerPosition.y, currPlayerPosition.z);
             } else{
                 Debug.Log("This shouldnt be seen, a spawnpoint is missing a direction");
             }
