c21eb748ea6570487d6e09a11c56fbb18f155708
MichaelCummins <Michael.Cummins1@marist.edu>
2020-06-20 22:16:51 -0400

    Adding enemy AIs

---
+++ UpdatedProject/Assets/Enemy_Lich.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy_Lich : MonoBehaviour
+{
+    public float health;
+    public float range;
+    public float speed;
+
+    public Transform target;
+
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+       // target = GameObject.FindGameObjectWithTag("Player").GetComponent<Transform>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(target.position.x - transform.position.x < 4){
+            transform.position = Vector2.MoveTowards(transform.position, target.position, speed * Time.deltaTime);
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D other) {
+        if(health == 0 ){
+            Destroy(gameObject);
+        }
+    }
+}
