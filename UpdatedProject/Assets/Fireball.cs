cd9185b5df788442617548c0e1aaf1c1063938e1
MichaelCummins <Michael.Cummins1@marist.edu>
2020-06-27 12:20:57 -0400

    Finalized fireball

--- UpdatedProject/Assets/Fireball.cs
+++ UpdatedProject/Assets/Fireball.cs
@@ -7,13 +7,21 @@ public class Fireball : MonoBehaviour
     public float speed = 3;
     public Rigidbody2D rb;
 
+    private float timeAlive = 0;
+    
+    private float lifeTime = 3;
+
     void Start(){
         rb.velocity = transform.right * speed;
+        Destroy(gameObject,lifeTime);
     }
 
+    void Update(){
+        timeAlive += Time.deltaTime;
+    }
 
     public void OnCollisionEnter2D(Collision2D collision){
-        if (collision.gameObject.name.Equals("Walls")){
+        if (collision.gameObject.tag.Equals("Wall") || collision.gameObject.tag.Equals("Enemy")){
             Destroy(gameObject);
         }
     }
