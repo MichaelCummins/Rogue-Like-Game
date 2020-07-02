ee79f878f97f6bf615a857696136ac1605cf2d13
MichaelCummins <Michael.Cummins1@marist.edu>
2020-07-01 22:42:23 -0400

    Creating enemy prefabs

---
+++ UpdatedProject/Assets/FireAtPlayer.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FireAtPlayer : MonoBehaviour
+{
+    public Transform target;
+    private Vector3 object_pos;
+    private float angle;
+}
---
+++ UpdatedProject/Assets/RotateAroundEnemy.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotateAroundEnemy : MonoBehaviour
+{
+    public Transform target;
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector2 dir = target.position - transform.position;
+        float angle = Mathf.Atan2(dir.y, dir.x) * Mathf.Rad2Deg;
+        Quaternion rotation = Quaternion.AngleAxis(angle, Vector3.forward);
+        transform.rotation = rotation;       
+    }
+}
