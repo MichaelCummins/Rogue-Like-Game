8d1c0d976176aaaf560d34aff732ceff417e5ae1
MichaelCummins <Michael.Cummins1@marist.edu>
2020-07-02 13:52:20 -0400

    Got Lich weapon to follow player correctly

--- UpdatedProject/Assets/FireAtPlayer.cs
+++ UpdatedProject/Assets/FireAtPlayer.cs
@@ -5,12 +5,18 @@ using UnityEngine;
 public class FireAtPlayer : MonoBehaviour
 {
     public Transform host;
-    public Transform target;
-    private Vector3 object_pos;
+
+    private Vector3 target;
     private Vector3 host_pos;
+
     private float angle;
 
     void Update(){
-        //host_pos = 
+        target = GameObject.FindGameObjectWithTag("Player").transform.position;
+        host_pos = Camera.main.WorldToScreenPoint(host.position);
+        target.x = target.x - host_pos.x;
+        target.y = target.y - host_pos.y;
+        angle = Mathf.Atan2(target.y, target.x) * Mathf.Rad2Deg;
+        transform.rotation = Quaternion.Euler(0, 0, angle);
     }
 }
