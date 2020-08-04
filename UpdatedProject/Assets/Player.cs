6c2628be279993b1cd182c529deb276389f1a429
MichaelCummins <Michael.Cummins1@marist.edu>
2020-08-03 20:48:02 -0400

    Break over, time to get back to work
    Made it so player doesnt reset position when not moving

--- UpdatedProject/Assets/Player.cs
+++ UpdatedProject/Assets/Player.cs
@@ -12,6 +12,8 @@ public class Player : MonoBehaviour
 
     public Animator animator;
     Vector2 movement;
+    private bool playerMoving;
+    private Vector2 lastMove;
 
     public GameObject SpellbookPanel;
     private int counter = 0;
@@ -25,13 +27,29 @@ public class Player : MonoBehaviour
 
     void Update(){
 
+        playerMoving = false;
+
         movement.x = Input.GetAxisRaw("Horizontal");
         movement.y = Input.GetAxisRaw("Vertical");
         rb.MovePosition(rb.position + movement * moveSpeed * Time.fixedDeltaTime);
 
+        if(movement.x > .5f || movement.x < -.5f){
+            animator.SetFloat("Horizontal", movement.x);
+            playerMoving = true;
+            lastMove = new Vector2(movement.x, movement.y);
+        }
+
+        if(movement.y > .5f || movement.y < -.5f){
+            animator.SetFloat("Vertical", movement.y);
+            playerMoving = true;
+            lastMove = new Vector2(movement.x, movement.y);
+        }
         animator.SetFloat("Horizontal", movement.x);
         animator.SetFloat("Vertical", movement.y);
         animator.SetFloat("Speed", movement.sqrMagnitude);
+        animator.SetBool("PlayerMoving", playerMoving);
+        animator.SetFloat("LastMoveX", lastMove.x);
+        animator.SetFloat("LastMoveY", lastMove.y);
 
         if(Input.GetKeyDown("space")){
             showhidePanel();
